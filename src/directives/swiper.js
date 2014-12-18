5bc7ac3e140d4966685152c4c223f380333de5ac
Tomasz Pietrzak <tomasz.pietrzak@nopattern.net>
2014-12-18 12:42:04 +0100

    feat(src): Add empty swiper directive.

---
+++ src/directives/swiper.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('4screens.engageform').directive( 'engageformSwiperDirective',
+  function() {
+    return {
+      restrict: 'C',
+      link: function () {
+      }
+    };
+  }
+);
