3e75f868a5bd2d344edb7b412b0d55f49e97354f
Tomasz Pietrzak <tomasz.pietrzak@nopattern.net>
2014-12-18 15:21:36 +0100

    feat(src): Add swiper in onfoPage.

--- src/directives/swiper.js
+++ src/directives/swiper.js
@@ -1,10 +1,31 @@
 'use strict';
 
 angular.module('4screens.engageform').directive( 'engageformSwiperDirective',
-  function() {
+  function( $timeout ) {
     return {
       restrict: 'C',
       link: function () {
+        var params = {
+          slideElement: 'article',
+          slidesPerView: 1,
+          pagination: '.pagination',
+          calculateHeight: true,
+          roundLengths: true,
+          resizeReInit: true
+        };
+
+        // This code will run after template has been loaded
+        // and transformed by directives
+        // and properly rendered by the browser
+        $timeout( function() {
+          $timeout( function() {
+            $timeout( function() {
+              $timeout( function() {
+                new Swiper( '.engageform-swiper-directive', params );
+              });
+            });
+          });
+        });
       }
     };
   }
