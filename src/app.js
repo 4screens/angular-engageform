0db37d3c3284c448edf2a91defb8280d93df320f
Tomasz Pietrzak <tomasz.pietrzak@nopattern.net>
2014-12-18 12:38:45 +0100

    feat(src): Add swiper and angular-youtube-embed library.

--- src/app.js
+++ src/app.js
@@ -2,5 +2,10 @@
 
 angular.module( '4screens.engageform',[
   '4screens.common',
-  '4screens.settings'
-]);
+  '4screens.settings',
+  'LocalStorageModule',
+  'youtube-embed'
+]).config(function( localStorageServiceProvider ) {
+  // localStorageService
+  localStorageServiceProvider.prefix = '4screens.engageform';
+});
