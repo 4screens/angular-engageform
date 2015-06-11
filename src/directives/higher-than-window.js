9b2f9b64f1b68ff8de7d71b193ddda62164b8c12
Artur Leśniewski <artur.lesniewski@nopattern.net>
2015-06-11 14:19:44 +0200

    chore(js): Remove warnings form js file.

--- src/directives/higher-than-window.js
+++ src/directives/higher-than-window.js
@@ -1,3 +1,5 @@
+'use strict';
+
 angular.module('4screens.engageform').directive( 'higherThanWindow', function( $document, $window ) {
   var $body = angular.element( $document.find('body').eq( 0 ) );
 
@@ -5,11 +7,11 @@ angular.module('4screens.engageform').directive( 'higherThanWindow', function( $
     link: function( scope, element ) {
       scope.$applyAsync( function() {
         if (element[0].clientHeight > $window.innerHeight) {
-          $body.addClass('higher-than-window')
+          $body.addClass('higher-than-window');
         } else {
-          $body.removeClass('higher-than-window')
+          $body.removeClass('higher-than-window');
         }
-      })
+      });
     }
-  }
+  };
 });
