af716161dbcadc253a2e02198987305214769f50
Mateusz Kocz <mateusz.kocz@nopattern.net>
2015-06-25 16:01:05 +0200

    feat(js): posting message when fullscreen is opened.

--- src/services/open-in-fullscreen.js
+++ src/services/open-in-fullscreen.js
@@ -6,12 +6,16 @@ angular.module('4screens.engageform')
     return function() {
       if (body.requestFullscreen) {
         body.requestFullscreen();
+        message.send('opened-in-fullscreen');
       } else if (body.msRequestFullscreen) {
         body.msRequestFullscreen();
+        message.send('opened-in-fullscreen');
       } else if (body.mozRequestFullScreen) {
         body.mozRequestFullScreen();
+        message.send('opened-in-fullscreen');
       } else if (body.webkitRequestFullscreen) {
         body.webkitRequestFullscreen();
+        message.send('opened-in-fullscreen');
       } else {
         message.send('request-fullscreen');
       }
