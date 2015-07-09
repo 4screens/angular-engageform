f0bfb46ecbdb6d5af52c3f87073eae99633d9d9f
Marcin Marzewski <marcin.marzewski@nopattern.net>
2015-07-09 11:54:07 +0200

    feat(dest): Build dest files.

--- engageform.js
+++ engageform.js
@@ -346,7 +346,8 @@ angular.module('4screens.engageform').controller( 'engageformDefaultCtrl',
         w,
         Math.round( trs.containerHeight * ( w / baseWidth ) ),
         Math.round( ( w / baseWidth ) * Math.round( baseWidth * trs.left * -1 / 100 ) || 0 ),
-        Math.round( ( w / baseWidth ) * Math.round( trs.containerHeight * trs.top * -1 / 100 ) || 0 )
+        Math.round( ( w / baseWidth ) * Math.round( trs.containerHeight * trs.top * -1 / 100 ) || 0 ),
+        dpr
       );
     };
 
@@ -1247,23 +1248,30 @@ angular.module('4screens.engageform').factory( 'EngageformBackendService',
 );
 
 'use strict';
-
-angular.module('4screens.engageform').factory( 
+// //res.cloudinary.com/fourscreens/image/fetch/
+angular.module('4screens.engageform').factory(
   'CloudinaryService',
   [ 'CONFIG',
     function ( CONFIG ) {
-      // cloudinaryUrl - Cloudinary cloud name, this need to be added to your CONFIG file
-      var cloudinaryUrl = CONFIG.backend.cloudinaryUrl || 'http://res.cloudinary.com/test4screens/image/fetch/';
+      var accountName = CONFIG.backend.cloudinary.account || 'test4screens'
+        , uploadFolderName = CONFIG.backend.cloudinary.uploadFolderName || 'console'
+        , imageUrlPrefix = 'http://res.cloudinary.com/' + accountName + '/image/'
+        , remoteMediaUrlPrefix = CONFIG.backend.domain.replace( ':subdomain', '' ) + CONFIG.backend.imagesUrl;
+
+      function correctUrl( prms, src ) {
+        return imageUrlPrefix + ( src.indexOf( remoteMediaUrlPrefix ) !== -1 ? 'upload/' : 'fetch/' ) + prms + '/' + src.replace( remoteMediaUrlPrefix, uploadFolderName );
+      }
 
       return {
         getImgUrl: function ( src, w, dpr, blur ) {
-          var blur = typeof blur !== 'undefined' && blur !== '0' ? ',e_blur:' + parseInt( blur, 10 ) * 100 + '/' : '/'
+          var blur = typeof blur !== 'undefined' && blur !== '0' ? ',e_blur:' + parseInt( blur, 10 ) * 100 : ''
           , dpr = ',dpr_' + dpr;
-          
-          return cloudinaryUrl + 'w_' + parseInt( w, 10 ) + ',c_limit' + dpr + blur + src;
+
+          return correctUrl( 'w_' + parseInt( w, 10 ) + ',f_auto,q_75' + dpr + ',c_limit' + blur, src );
         },
-        getMainImgUrl: function ( src, sw, w, h, ox, oy ) {
-          return cloudinaryUrl + 'w_' + parseInt( sw, 10 ) + '/' + 'w_' + w + ',h_' + h + ',x_' + ox + ',y_' + oy + ',c_crop/' + src;
+        getMainImgUrl: function ( src, sw, w, h, ox, oy, dpr ) {
+          dpr = typeof dpr === 'undefined' ? '' : ',dpr_' + dpr;
+          return correctUrl( 'w_' + parseInt( sw, 10 ) + ',f_auto,q_75' + dpr + '/' + 'w_' + w + ',h_' + h + ',x_' + ox + ',y_' + oy + ',c_crop', src );
         }
       };
     } ]
