754b3104ee05fe13b0efa4c4e0f8f4e36f86ce6d
Tomasz Pietrzak <tomasz.pietrzak@nopattern.net>
2015-08-20 13:17:50 +0200

    feat(release): New build files.

--- engageform.js
+++ engageform.js
@@ -1,6 +1,6 @@
 (function(angular) {
 /*!
- * 4screens-angular-engageform v0.2.14
+ * 4screens-angular-engageform v0.2.11
  * (c) 2015 Nopattern sp. z o.o.
  * License: proprietary
  */
@@ -404,8 +404,10 @@ var Meta;
             this.globalTitle = '';
             this.globalDescription = '';
             this._engageform = engageform;
-            this.globalTitle = this._engageform.settings.share.title || '';
-            this.globalDescription = this._engageform.settings.share.description || '';
+            if (this._engageform.settings.share) {
+                this.globalTitle = this._engageform.settings.share.title || '';
+                this.globalDescription = this._engageform.settings.share.description || '';
+            }
             if (this._engageform.endPages.length < 1 || !_.find(this._engageform.pages, { social: true })) {
                 if (this._engageform.startPages.length && this._engageform.pages[this._engageform.startPages[0]].title) {
                     this.globalTitle = this._engageform.pages[this._engageform.startPages[0]].title;
