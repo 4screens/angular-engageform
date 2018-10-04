f20e44177d0b2ab129123376bfc2f7233985c40a
Mateusz Kocz <mateusz@kocz.pl>
2018-10-04 15:10:11 +0200

    Add semicolons

--- src/page/type/poster.ts
+++ src/page/type/poster.ts
@@ -1,9 +1,9 @@
 module Page {
   export class Poster extends Page {
     type = Type.Poster;
-    private button: string
-    private exitLink: boolean
-    private link: string
+    private button: string;
+    private exitLink: boolean;
+    private link: string;
 
     constructor(engageform: Engageform.IEngageform, data: API.IQuizQuestion) {
       super(engageform, data);
