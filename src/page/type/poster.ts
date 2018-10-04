694cf8a6e0bce569f57fc8c6e94d1053e6c58f50
Mateusz Kocz <mateusz@kocz.pl>
2018-10-04 15:07:41 +0200

    Add cover data to the poster page

--- src/page/type/poster.ts
+++ src/page/type/poster.ts
@@ -1,9 +1,17 @@
 module Page {
   export class Poster extends Page {
     type = Type.Poster;
+    private button: string
+    private exitLink: boolean
+    private link: string
 
     constructor(engageform: Engageform.IEngageform, data: API.IQuizQuestion) {
       super(engageform, data);
+      if (data.coverPage) {
+        this.button = data.coverPage.buttonText;
+        this.exitLink = data.coverPage.exitLink;
+        this.link = data.coverPage.link;
+      }
     }
   }
 }
