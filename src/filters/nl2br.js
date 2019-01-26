6e8916b8116f7bd60a78aecb44243e98183e4450
Mateusz Kocz <mateusz@kocz.pl>
2019-01-26 11:10:40 +0100

    Remove old files

--- src/filters/nl2br.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('4screens.engageform').filter( 'nl2br', function( $sce ) {
-  return function( message ) {
-    return $sce.trustAsHtml(
-      ( message + '' ).replace( /([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1<br />$2' )
-    );
-  };
-});
